5005da4d7c43e0856c9f8bcb01f1c5478a3c5f4f
ElenaIvankina <elenaryzhova1991@yandex.ru>
2021-12-01 14:30:50 +0300

    Initial

---
+++ VK_SwiftUI/VK_SwiftUI/VK_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VK_SwiftUIApp.swift
+//  VK_SwiftUI
+//
+//  Created by Елена Иванкина on 01.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct VK_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
