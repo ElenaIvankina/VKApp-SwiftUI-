69b74d41aae1a38e436f70475a4507dfe2df1083
ElenaIvankina <elenaryzhova1991@yandex.ru>
2021-12-02 10:29:58 +0300

    HW1

--- VK_SwiftUI/VK_SwiftUI/ContentView.swift
+++ VK_SwiftUI/VK_SwiftUI/ContentView.swift
@@ -8,9 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var login = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.blue.ignoresSafeArea()
+            ScrollView{
+                VStack{
+                    Text("FriendsApp")
+                        .padding(.top, 32)
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                HStack{
+                        Text("Логин")
+                            .foregroundColor(.white)
+                        Spacer()
+                        TextField("", text: $login)
+                            .frame(maxWidth: 150)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            
+                    }
+                HStack{
+                        Text("Пароль")
+                            .foregroundColor(.white)
+                        Spacer()
+                        SecureField("", text: $password )
+                            .frame(maxWidth: 150)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            
+                    }
+                    
+                    Button(action: {print ("Hello")}) {
+                        Text("Log in")
+                            .foregroundColor(.white)
+                    }
+                    .padding(.top, 20)
+                    .padding(.bottom, 50)
+                    .disabled(login.isEmpty || password.isEmpty)
+                    
+                    Spacer()
+                }
+                .frame(maxWidth: 250)
+            }
+            
+        }
+
+
+        
     }
 }
 
